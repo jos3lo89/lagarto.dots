5b121a9d96d0403daaf37ef93158a59b400ff189
jose luis galindo cardenas <126422481+jos3lo89@users.noreply.github.com>
2025-02-22 14:33:51 -0500

    Update logo.lua

--- nvim/plugins/logo.lua
+++ nvim/plugins/logo.lua
@@ -1,7 +1,33 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    picker = {
+      matcher = {
+        fuzzy = true,
+        smartcase = true,
+        ignorecase = true,
+        filename_bonus = true,
+      },
+      sources = {
+        explorer = {
+          matcher = {
+            fuzzy = true, -- Enables fuzzy matching, so you can be a bit imprecise with your search terms
+            smartcase = true, -- If your search term has uppercase letters, the search becomes case-sensitive
+            ignorecase = true, -- Ignores case when searching, unless smartcase is triggered
+            filename_bonus = true, -- Gives a higher priority to matches in filenames
+            sort_empty = false, -- If no matches are found, it won't sort the results
+          },
+        },
+      },
+    },
     dashboard = {
+      sections = {
+        { section = "header" },
+        -- { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
       preset = {
         header = [[
      ██╗ ██████╗ ███████╗██████╗ ██╗      ██████╗
@@ -10,7 +36,23 @@ return {
 ██   ██║██║   ██║╚════██║ ╚═══██╗██║     ██║   ██║
 ╚█████╔╝╚██████╔╝███████║██████╔╝███████╗╚██████╔╝
  ╚════╝  ╚═════╝ ╚══════╝╚═════╝ ╚══════╝ ╚═════╝
-        ]],
+]],
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
       },
     },
   },
